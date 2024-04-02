b52119a7af6422c10ccd1332abf3fb48076c3bf1
matheusbtguerra <146946503+matheusbtguerra@users.noreply.github.com>
2024-04-02 11:43:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function converter() {
+    const numInput = document.getElementById("numInput");
+    const coversao = numInput.value * 5.05;
+    const resultado = document.getElementById("resultado");
+
+    resultado.innerHTML = "O resultado da sua conversão é: " + coversao;
+}
+
+const toggleBtn = document.getElementById("toggle");
+const body = document.body;
+
+toggleBtn.addEventListener('click', function() {
+    body.classList.toggle('drk')
+})
